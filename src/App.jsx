f69b8b40987a4f838e00275094925569ac95b94f
Raiki Rani <raikiranirajput@gmail.com>
2026-07-17 14:55:41 +0530

    Fixed Sidebar Logo import

--- src/App.jsx
+++ src/App.jsx
@@ -178,7 +178,7 @@ function App() {
                             <Reports />
                         </ProtectedRoute>
                     }
-                />
+                    />
 
                 <Route
                     path="/track"
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Sidebar() {
